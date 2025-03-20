27675d805116c1668f765e4162ff6f62e46255bf
amitgaurav6721 <54430719+amitgaurav6721@users.noreply.github.com>
2025-03-20 19:40:13 +0530

    Delete matrix.bat

--- matrix.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:loop
-set /a num=(%Random% %%9)+1
-color %num%
-curl https://raw.githubusercontent.com/amitgaurav6721/papa-image/refs/heads/main/test.txt?token=GHSAT0AAAAAADA24GANLT4XHLTEIZD2SBXEZ64EBBApause
-goto :loop
